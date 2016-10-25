9cfa2aa979d5e810796c4b9a6d648ee3e10a6358
Pawellos <pablox377@gmail.com>
2016-10-25 15:36:40 +0200

    header - suma

--- prog.cpp
+++ prog.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdio.h>
+#include "funkcje.h"
 
 using namespace std;
 
@@ -8,5 +9,13 @@ int main()
   cout << "Hello World!" << endl;
   cout << "czyli witaj zjebany swiecie" << endl;
   cout << "zartowalem nie jest zjebany tylko trudny" << endl;
+int pierwsza;
+int druga;
+
+  cout << "Podaj 1 liczbe" <<endl;
+  cin >> pierwsza; 
+  cout << "podaj 2 liczbe" <<endl;
+  cin >> druga;
+  cout << "wynik = " << suma(pierwsza,druga)<< endl;
   return 0;
 }
